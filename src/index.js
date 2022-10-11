c190a0cb9214f1c6617d1456ab07d44e430291eb
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-10-11 09:55:13 +0200

    created node server
    - changed html to ejs
    - added public folder
    - added views folder
    - store input to memory
    - display input on output
    
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 8000
+let status = {
+
+}
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+  
+app.get('/input', (req, res) => {
+  res.render("input")
+})
+
+
+app.get("/output", (req, res) => {
+  res.render("output", {status: status})
+})
+
+app.post("/sendinput", (req, res) => {
+  console.log(req.body.inputfield)
+  status[req.body.inputfield] = req.body.inputfield
+  res.redirect("/input")
+})
+
+app.listen(port, () => {
+    console.log(`Närvaroadmin körs på port ${port}`)
+})
