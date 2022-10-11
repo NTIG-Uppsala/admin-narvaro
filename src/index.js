9db7b9001aeb76063b67256d8a3478968ba3dd53
linus-jansson <contact@limpan.dev>
2022-10-11 13:46:30 +0200

    fixed duplicate message bug
    Co-authored-by: Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
    Co-authored-by: Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
    Co-authored-by: Kasper-coder <kasper.holgersson@elev.ga.ntig.se>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 8000
-let status = {
+let status = []
 
-}
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
@@ -21,14 +20,26 @@ app.get('/input', (req, res) => {
   res.render("input")
 })
 
-
 app.get("/output", (req, res) => {
   res.render("output", {status: status})
 })
 
 app.post("/sendinput", (req, res) => {
-  console.log(req.body.inputfield)
-  status[req.body.inputfield] = req.body.inputfield
+  try {
+    console.log(req.body.inputfield)
+    date = new Date()
+    Cdate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+
+    status.push({
+      status_text: req.body.inputfield,
+      current_hour: Cdate
+    })
+
+    console.log(status)
+  } catch (error) {
+
+  }
+  
   res.redirect("/")
 })
 
