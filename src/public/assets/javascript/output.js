1138be6413739a7c9c7dbde31b312b7ad10f808d
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-10-12 15:37:07 +0200

    added styling
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Gustavsson123 <111958017+Gustavsson123@users.noreply.github.com>

--- src/public/assets/javascript/output.js
+++ src/public/assets/javascript/output.js
@@ -13,18 +13,23 @@ function escapeHTML(unsafe_str) {
 /* When document has loaded in */
 $(document).ready(function() {
     const socket = io();
+    let newHTML;
     /* On a satus update event from the server, create new element with data */
     socket.on("status update", (dataArray) => {
         /* Set innerhtml of statusDiv */
         console.log("status update", dataArray);
-        let newHTML = "";
+        newHTML = "";
         dataArray.forEach((person) => {
-            newHTML += `
-                <div class="message-container">
+            console.log(person)
+            if (person.name.length > 0) {
+                newHTML += `
+                <div class="message-container" id="message-containerOutput">
                     <p>${person.name}</p>
                     <span>${(person.status == true) ? "tillgänglig" : "ej tillgänglig"}</span>
                 </div>
             `
+            }
+            
         })
         $("#statusDiv").html(newHTML);
     })
