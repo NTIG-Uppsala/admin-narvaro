530e8c4a38bb63291fe0c89e58b986d037e46e34
linus-jansson <contact@limpan.dev>
2022-10-12 15:38:35 +0200

    function which returns current date
    Co-authored-by: Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
    Co-authored-by: Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
    Co-authored-by: Kasper-coder <kasper.holgersson@elev.ga.ntig.se>

--- src/index.js
+++ src/index.js
@@ -14,91 +14,102 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+/* Function which returns the current date in preferred format */
+const current_date = () => {
+    let date = new Date()
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${((date.getMinutes() < 10) ? '0' : '') + date.getMinutes()}:${((date.getSeconds() < 10) ? '0' : '') + date.getSeconds()}`
+}
+
 /* Handles a POST request to /sendinput */
-let status = [
+let statusArray = [
     {
         name: "Mathias Laveno",
         role: "Rektor",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     },
     {
         name: "Henrik Jonsson",
         role: "biträdande rektor",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     },
     {
         name: "Sara Hagberg",
         role: "biträdande rektor",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     },
     {
         name: "Maud Enbom",
         role: "Skolsköterska",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     },
     {
         name: "Therese Ekman",
         role: "Administratör",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     },
     {
         name: "Vincent Persson",
         role: "Tekniker",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     },
     {
         name: "Megan Gallagher Sundström",
         role: "Kurator",
         locked: false,
-        status: false
+        status: false,
+        latest_change: current_date()
     }
 ]
 
 /* notifies console when someone connects to server socket */
 io.on('connection', (socket) => { 
   console.log('A user connected', socket.handshake.address);
-  /* A event for changes to the input */
+
+  /* A event for changes to the status */
   socket.on('status change', (dataArray) => { 
     console.log("STATUS CHANGE")
-    
-    let date = new Date()
-
-    let latest_change = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${((date.getMinutes() < 10) ? '0' : '') + date.getMinutes()}:${((date.getSeconds() < 10) ? '0' : '') + date.getSeconds()}`
-    
     /* Update status array */
-    for (const key in status) {
+    for (const key in statusArray) {
         let index = key;
-        let status_object = status[key];
+        let status_object = statusArray[key];
 
         for (const key in dataArray) {
             let key_name = key;
             let input_object = dataArray[key];
             
+            /* Prevent duplicate names */
             if (status_object.name == input_object.name) {
                 /* Only update if change */
                 if (status_object.status != input_object.status) {
                     status_object.status = input_object.status;
-                    status_object["latest_change"] = latest_change;
+                    status_object["latest_change"] = current_date();
                 }
 
                 if (status_object.locked != input_object.locked) {
                     status_object.locked = input_object.locked;
-                    status_object["latest_change"] = latest_change;
+
+                    status_object["latest_change"] = current_date();
                 }    
             }
 
             
         }
     }
-    console.log(status)
+    console.log(statusArray)
     /* Status change */
-    io.emit("status update", status)
+    io.emit("status update", statusArray)
 
   });
 
@@ -106,12 +117,12 @@ io.on('connection', (socket) => {
 
 /* Shows input page on root */
 app.get('/', (req, res) => {
-    res.render("input", {statusObject: status})
+    res.render("input", {statusObject: statusArray})
 })
 
 /* Shows output page on /output */
 app.get("/output", (req, res) => {
-  res.render("output", {statusObject: status})
+  res.render("output", {statusObject: statusArray})
 })
 
 
