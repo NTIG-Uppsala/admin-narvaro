a04c06ca2049761f623dd853412198586609eea0
linus-jansson <contact@limpan.dev>
2022-10-12 13:52:40 +0200

    live updates
    Co-authored-by: Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
    Co-authored-by: Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
    Co-authored-by: Kasper-coder <kasper.holgersson@elev.ga.ntig.se>

--- src/index.js
+++ src/index.js
@@ -12,26 +12,86 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
+
+/* Handles a POST request to /sendinput */
+let status = [
+    {
+        name: "Mathias Laveno",
+        title: "Rektor",
+        locked: false,
+        status: null
+    },
+    {
+        name: "Henrik Jonsson",
+        title: "biträdande rektor",
+        locked: false,
+        status: null
+    },
+    {
+        name: "Sara Hagberg",
+        title: "biträdande rektor",
+        locked: false,
+        status: null
+    },
+    {
+        name: "Maud Enbom",
+        title: "Skolsköterska",
+        locked: false,
+        status: null
+    },
+    {
+        name: "Therese Ekman",
+        title: "Administratör",
+        locked: false,
+        status: null
+    },
+    {
+        name: "Vincent Persson",
+        title: "Tekniker",
+        locked: false,
+        status: null
+    },
+    {
+        name: "Megan Gallagher Sundström",
+        title: "Kurator",
+        locked: false,
+        status: null
+    }
+]
 
 /* notifies console when someone connects to server socket */
 io.on('connection', (socket) => { 
   console.log('A user connected', socket.handshake.address);
+  /* A event for changes to the input */
+  socket.on('status change', (dataArray) => { 
+    console.log("STATUS CHANGE")
+    
+    let date = new Date()
+
+    let latest_change = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${((date.getMinutes() < 10) ? '0' : '') + date.getMinutes()}`
+    // dataArray["latest_change"] = latest_change
+    status = dataArray
+    console.log(dataArray)
+    /* Status change */
+    io.emit("status update", dataArray)
+
+  });
+
 });
+
+
 
 /* Shows input page on root */
 app.get('/', (req, res) => {
-    res.render("input")
+    res.render("input", {statusObject: status})
 })
 
 /* Shows output page on /output */
 app.get("/output", (req, res) => {
-  res.render("output", {status: status})
+  res.render("output", {statusObject: status})
 })
 
-
-/* Handles a POST request to /sendinput */
-let status = []
 app.post("/sendinput", (req, res) => {
   let date = new Date()
   let cDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${((date.getMinutes() < 10) ? '0' : '') + date.getMinutes()}`
