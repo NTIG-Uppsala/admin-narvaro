4abf863fd1238ed48f7c507add428d29c724cb34
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-10-13 10:50:41 +0200

    update live change html
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Gustavsson123 <111958017+Gustavsson123@users.noreply.github.com>

--- src/public/assets/javascript/output.js
+++ src/public/assets/javascript/output.js
@@ -7,13 +7,21 @@ $(document).ready(function() {
         /* Set innerhtml of statusDiv */
         console.log("status update", dataArray);
         newHTML = "";
+        let id = 0;
         dataArray.forEach((person) => {
+            id++;
             console.log(person)
             if (person.name.length > 0) {
                 newHTML += `
-                <div class="message-container" id="message-containerOutput">
-                    <p>${person.name}</p>
-                    <span style="color:${(person.status == true) ? "#66bb6a" : "red"}">${(person.status == true) ? "Tillgänglig" : "Ej tillgänglig"}</span>
+                <div class="message-container ${(id%2==0) ? 'gray-color': ''}" id="message-containerOutput">
+                    <div class="name">
+                        <span>${person.name }</span>
+                        <span class="role">${ person.role }</span>
+                    </div>
+                    <div class="status">
+                        <span style="color: ${(person.status == true) ? '#66bb6a' : 'red'} ">${(person.status == true) ? "Tillgänglig" : "Ej tillgänglig"}</span>
+                        <span class="latest-update">${person.latest_change}</span>
+                  </div>  
                 </div>
             `
             }
