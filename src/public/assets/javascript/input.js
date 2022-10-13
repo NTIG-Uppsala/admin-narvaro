fa0b8c7b4f4db62f13b40931d5533c49006da7f8
linus-jansson <contact@limpan.dev>
2022-10-13 11:43:22 +0200

    Update setstatus page for other users if changed
    Co-authored-by: Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
    Co-authored-by: Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
    Co-authored-by: Kasper-coder <kasper.holgersson@elev.ga.ntig.se>

--- src/public/assets/javascript/input.js
+++ src/public/assets/javascript/input.js
@@ -1,6 +1,39 @@
 /* When document has loaded in */
 $(document).ready( () => {
     const socket = io();
+    /* listen on updates from other users */
+    socket.on("status update", (dataArray) => {
+        console.log("status update", dataArray);
+
+        /* Update checkboxses */
+        dataArray.forEach((person) => {
+            $("div[name='main']").children().each((index, element) => {
+
+                let person_element = $(element).find("span[name='personName']");
+                let status_element = $(element).find('input[name="avaliableCheckbox"]')
+                let locked_element = $(element).find('input[name="lockCheckbox"]')
+
+                if (person_element.text() == person.name) {
+                    if (person.locked) {
+                        status_element.prop('disabled', true)
+                        locked_element.prop('checked', true)
+                    }
+                    else {
+                        status_element.prop('disabled', false)
+                        locked_element.prop('checked', false)
+                    }
+        
+                    if (person.status) {
+                        status_element.prop('checked', true)
+                    }
+                    else {
+                        status_element.prop('checked', false)
+                    }
+                }
+            });
+        });
+
+    });
 
     $("div[name='main']").change(() => {
         let status_array = []
@@ -12,10 +45,10 @@ $(document).ready( () => {
             let locked_element = $(element).find('input[name="lockCheckbox"]')
 
             if (locked_element.is(':checked')) {
-                status_element.attr('disabled','disabled')
+                status_element.prop('disabled', true)
             }
             else {
-                status_element.removeAttr('disabled')
+                status_element.prop('disabled', false)
             }
 
             let data = {
@@ -27,7 +60,7 @@ $(document).ready( () => {
             console.log(data)
             status_array.push(data)
         });
-        console.log("changed");
+        console.log("Detcted local changed");
         
         /* Push all data through websocket */
         socket.emit("status change", status_array)
