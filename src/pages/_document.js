7b114b1c13da75316bba4cc9872752b62aeb954e
linus-jansson <contact@limpan.dev>
2022-10-17 14:54:07 +0200

    rewrite nextjs
    Co-authored-by: Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
    Co-authored-by: Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
    Co-authored-by: Kasper-coder <kasper.holgersson@elev.ga.ntig.se>

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+        <Head>
+        <link rel="icon" href="nti_logo_footer.svg" sizes="any" type="image/svg+xml" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/setstatus.js
@@ -0,0 +1,112 @@
+import Link from 'next/link';
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import io from 'socket.io-client';
+import moment from 'moment';
+
+moment.locale('sv')
+
+
+const Input = () => {
+    const socket = io();
+    
+    const [statusArray, setStatusArray] = useState([]);
+
+    // fetch('http://localhost:3000/api/persons')
+    useEffect(() => {
+        console.log("i fire once");
+
+        axios.get('/api/getstatus').then(res => {
+            console.log(res.data)
+            return render_people(res.data)
+        });
+
+        socket.on('status update', (response) => {
+            console.log(response)
+            console.log("STATUS UPDATE")
+            return render_people(response)
+        });
+    }, []);
+
+    const handleCheckboxChange = (event) => {
+        let return_value = {
+            name: event.target.name,
+            status: event.target.checked
+        }
+        socket.emit("status change", return_value)
+        
+    }
+
+    const render_people = (person_object) => {
+
+        let people_elements = [];
+
+        person_object.forEach((item, index) => {
+            // console.log("forloop")
+            
+            console.log(item)
+            people_elements.push(
+            <Person
+                key={index}
+                name={item.name}
+                status={item.status}
+            />)
+            
+        
+        });
+
+        setStatusArray(people_elements)
+    }
+
+    const Person = (props) => {
+        return (
+            <div className="message-container" name="personDiv">
+
+                <div className="name">
+                    <span name="personName">{props.name}</span>
+                </div>
+                <div className="container">
+                    <label className="switch" htmlFor={'avaliable-' + props.name.replace(" ", "-")}>
+                        {(props.status == true) ? 
+                            <input type="checkbox" name={props.name} id={'avaliable-' + props.name.replace(" ", "-")} onChange={handleCheckboxChange} checked={props.status}/>
+                        : 
+                            <input type="checkbox" name={props.name} id={'avaliable-' + props.name.replace(" ", "-")} onChange={handleCheckboxChange} checked={props.status} />
+                        }
+                        
+                        <div className="slider round"></div>
+                    </label>
+                </div>
+            </div>
+
+            
+        )
+
+    }
+
+    return (
+      <>
+        <div className="backgroundImage">
+          <img src="images/backgroundNTI.jpg" />
+        </div>
+  
+        <img id="logo" src="images/nti_logo_footer.svg" alt="" />
+        
+        <div className="message-container-wrapper">
+          <div name="main">
+              <h1>Ange din status</h1>
+                {statusArray}
+          </div>
+          <div>
+            <Link href="/">
+                <a>statis</a>
+            </Link>
+          </div>
+          
+  
+        </div> 
+      </>
+         
+    );
+  }
+  
+  export default Input;
