28f12afbdce4e42a694fa9218d6b9bb5f49c29b8
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-10-24 14:45:05 +0200

    Only filter on other users if there is other users
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,5 @@
 import Router from 'express';
 
-import mongoose from 'mongoose';
 import Database from '../Database.js';
 
 const database_instance = new Database();
@@ -31,8 +30,11 @@ apiRouter.post('/updateusers', (req, res) => {
     req.body.forEach(user => {
         console.log(user)
         console.log("Updating user", user.name)
-        console.log("New values", user)
-        // database_instance.update_user(user.objectId, user);
+        console.log("New values", user)        
+        
+        database_instance.update_user(user.objectId, user)
+        .then((updat_result) => {console.log("Updated user, result ->", updat_result)})
+        .catch((err) => {throw err});
     })
     req.io.emit("status update")
     return res.status(200).json({status: "ok"})
@@ -63,31 +65,34 @@ apiRouter.post('/verifyurl', async (req, res) => {
         try {
             
             let privilege = await database_instance.get_privileges({_id: user_result.privilege})
-            let tmp;
+            let other_users;
 
             /* Depending on privilege get different users */
             switch (privilege.control) {
                 case 1: // only change the person himself
-                    tmp = await database_instance.get_users({name: user_result.name})
+                    other_users = await database_instance.get_users({name: user_result.name})
                     break;
                 case 2: // change all people in same group
-                    tmp = await database_instance.get_users({group: user_result.group})
+                    other_users = await database_instance.get_users({group: user_result.group})
                     break;
                 case 3: // change all people
-                    tmp = await database_instance.get_users({})
+                    other_users = await database_instance.get_users({})
                     break;
                 default:
-                    tmp = [] // no users
+                    other_users = [] // no users
                     break;
             }
 
-            /* Filter out already existing user in user_array */
-            tmp = tmp.filter(( obj ) => {
-                return obj.name != user_result.name;
-            });
-
-            /* Concat user_array with tmp */
-            user_array = user_array.concat(tmp)
+            /* If user can see more than him/herself */
+            if (other_users.length > 0) {
+                /* Filter out already existing user in user_array */
+                other_users = other_users.filter(( obj ) => {
+                    return obj.name != user_result.name;
+                });
+                /* Concat user_array with tmp */
+                user_array = user_array.concat(other_users)
+            }
+            
 
         } catch (error) {
             throw error
