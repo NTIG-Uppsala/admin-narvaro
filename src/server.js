5723464cfd30abab9211b9d08de195eee49a5bf8
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-10-24 14:59:34 +0200

    added dev url
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ nextApp.prepare().then( async () => {
         if (err) throw err
         console.log("Server is running on port 8000")
     });
-
+    const database_url = (process.env.NODE_ENV == "production") ? process.env.MONGODB_URI : process.env.MONGODB_URI_DEV;
     mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASSWORD)}@admin-narvaro.o11easj.mongodb.net/users`, (err) => {
         if (err) throw err;
         
