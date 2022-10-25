66d4ca415a8a963d644bb63d8e88ed14a74f5732
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-10-25 11:19:25 +0200

    Updated formatting
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Gustavsson123 <111958017+Gustavsson123@users.noreply.github.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script strategy='beforeInteractive' src="https://cdn.socket.io/4.5.0/socket.io.min.js" integrity="sha384-7EyYLQZgWBi67fBtVxw60/OWl1kjsfrPFcaU0pp0nAh+i8FD068QogUvg85Ewy1k" crossOrigin="anonymous"/>
       <Script strategy='beforeInteractive' src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossOrigin="anonymous" />
-
       <Component {...pageProps} />
     </>
   )
