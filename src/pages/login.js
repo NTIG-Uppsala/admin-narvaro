1a445eb6235c9b267e7b0048b98c5890889eb3e1
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2022-10-25 15:59:17 +0200

    Added css and fixed password visibility
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Gustavsson123 <111958017+Gustavsson123@users.noreply.github.com>

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,6 +21,20 @@ export default () => {
         console.log(password)
     }
 
+    /* Changes password visibility */
+
+    const passwordState = () => {
+        var inputField = document.getElementById("password");
+        var showPasswordButton = document.getElementById("show-password");
+        if (inputField.type === "password") {
+            inputField.type = "text";
+            showPasswordButton.style.backgroundImage = "url('/images/eye-dash.svg')";
+        } else {
+            inputField.type = "password";
+            showPasswordButton.style.backgroundImage = "url('/images/eye.svg')";
+        }
+      }
+
     return (
         <>
             <div className="backgroundImage">
@@ -29,15 +43,18 @@ export default () => {
 
             <img id="logo" src="images/nti_logo_footer.svg" alt="" />
             <div className="grid-center">
-                <div className="message-container-wrapper">
+                <div className="login-container-wrapper">
                     <div className="login-container">
                         <h1>Logga In</h1>
                         <div className="password">
-                            <input type="password" id="password" name='password' onChange={updateState} placeholder="Lösenord" /><br />
+                            <div className='buttonIn'>
+                                <input type="password" id="password" name='password' onChange={updateState} placeholder="Lösenord" /><br />
+                                <button id="show-password" onClick={passwordState}></button>
+                            </div>
                             <div className="incorrect-password">
                                 {correctPassword === false ?
                                     <>
-                                        <img src="/images/danger.svg" alt="picture of eye" />
+                                        <img src="/images/danger.svg" alt="varningstecken" />
                                         <span>Fel lösenord</span>
                                     </>
                                     : null
