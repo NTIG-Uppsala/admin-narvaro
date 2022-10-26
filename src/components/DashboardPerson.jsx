a02a89ffcfee8a5e8e89cb7db1814d6acfc76bdf
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-10-26 11:17:03 +0200

    changing state
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>

--- src/components/DashboardPerson.jsx
+++ src/components/DashboardPerson.jsx
@@ -1,20 +1,46 @@
 import GroupOptions from "./DashboardGroupOptions"
 import PrivilegeOptions from "./DashboardPrivilegeOptions"
+import React, { useState, useEffect } from 'react'
 const Person = (props) => {
+    const [person, setPerson] = useState(props)
+    const [changing, setChanging] = useState(false)
+
     return (
         <div className={"message-container message-container-flexwrap" + ((props.id % 2 == 0) ? ' gray-color' : '')} onChange={props.onChange}>
             <div className="name name-size">
-                <input type="text" required name="name" id={props._id} defaultValue={props.name}></input>
+                {
+                    changing ?
+                        <input type="text" name="name" value={person.name} onChange={(e) => setPerson({ ...person, name: e.target.value })} />
+                        :
+                        <p>{person.name}</p>
+                }
             </div>
             <div className="name name-size">
-                <input type="text" required name="role" id={props._id} defaultValue={props.role}></input>
+                {
+                    changing ?
+                        <input type="text" name="role" value={person.role} onChange={(e) => setPerson({ ...person, role: e.target.value })} />
+                        :
+                        <p>{person.role}</p>
+                }
             </div>
             <div className="container container-flexcenter">
-                <PrivilegeOptions name="privilege" id={props._id}  privilege_id={props.privilege_id} />
+                {
+                    changing ?
+                        <PrivilegeOptions name="privilege" id={props._id} privilege_id={props.privilege_id} />
+                        :
+                        <p>{person.privilege_id}</p>
+                }
+
             </div>
             <div className="container container-flexcenter">
-                <GroupOptions  name="group" id={props._id} group_id={props.group_id}/>
+                {
+                    changing ?
+                        <GroupOptions name="group" id={props._id} group_id={props.group_id} />
+                        :
+                        <p>{person.group_id}</p>
+                }
             </div>
+            <button onClick={() => { setChanging(!changing) }}>edit</button>
         </div>
     )
 }
