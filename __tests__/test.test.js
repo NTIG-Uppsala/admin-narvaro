5460a5127e41bf0b516d7fb82f14bc0576329102
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-10-27 14:06:28 +0200

    Fixed error in jest
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -2,12 +2,13 @@ import { render, screen } from '@testing-library/react'
 import Output from '../src/pages/index'
 import '@testing-library/jest-dom'
 
+// https://lightrun.com/answers/prisma-prisma-jest-27-errors-with-referenceerror-setimmediate-is-not-defined
+global.setImmediate = global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
+
 describe('Index page', () => {
   it('Checks if status text is rendered on page', async () => {
     render(<Output />)
     const found = await screen.findByText('Status')
-
-    console.log(found)
     expect(found).toBeInTheDocument()
   })
 })
