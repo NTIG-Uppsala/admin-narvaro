2e68dc2473f3388829a3f8e52f3ffcc82eecb189
Linus J <linus.jansson2000@gmail.com>
2022-10-28 13:10:49 +0200

    Use of GetServerSideProps when getting users and verified onload

--- src/pages/setstatus.js
+++ src/pages/setstatus.js
@@ -8,98 +8,119 @@ import Person from '../components/SetPersonStatus';
 
 import Head from 'next/head';
 
-const Input = () => {
+const Input = (props) => {
     const socket = io();
-    
     const [people, setPeople] = useState([]);
-    const [verified, setVerified] = useState(false);
+    const [verified, setVerified] = useState(props.verified);
 
     const router = useRouter()
 
     /* 
         When the Page is loaded
-        - Verify the url
         - add a socket listener for status update
     */
-
-    useEffect( () => {
+    useEffect(() => {
         if (router.isReady) {
-
-            console.log("i fire once");
-            console.log("router.query: ", router.query.auth);
-
-            axios.post('/api/verifyurl', {uri: router.query.auth}).then(res => {
-                console.log(res.data)
-                setVerified(res.data.verified)
-                render_people(res.data.users)
-            });
-
             socket.on('status update', () => {
                 console.log("STATUS UPDATE")
-                axios.post('/api/verifyurl', {uri: router.query.auth}).then(res => {
-                    console.log(res.data)
+                axios.post('/api/verifyurl', { uri: router.query.auth }).then(res => {
                     setVerified(res.data.verified)
-                    render_people(res.data.users)
+                    setPeople(() => {
+                        return render_people(res.data.users)
+                    })
+
                 });
             });
         }
     }, [router.isReady]);
 
+    useEffect(() => {
+        setPeople(() => {
+            return render_people(props.users)
+        })
+    }, [])
+
     /* Render the person */
     const render_people = (person_object) => {
-
-        if (person_object == null) return;
-        let people_elements = person_object.map((item, index) => {
-            return ( 
-                <Person
-                    key={index}
-                    _id={item._id}
-                    id={index+1}
-                    name={item.name}
-                    status={item.status}
-                />
-            )
-        })
-        setPeople(people_elements)
-        console.log("Status array::::", people)
+        if (person_object.length < 1) {
+            return [];
+        }
+        else {
+            return person_object.map((item, index) => {
+                return (
+                    <Person
+                        key={index}
+                        id={index + 1}
+                        _id={item._id}
+                        name={item.name}
+                        status={item.status}
+                    />
+                )
+            })
+        };
     }
 
+
     return (
-      <>
-        <Head>
-            <title>Sätt status</title>
-        </Head>
+        <>
+            <Head>
+                <title>Sätt status</title>
+            </Head>
 
-        <div className="backgroundImage">
-          <img src="images/backgroundNTI.jpg" alt="bakgrunds bild på hemsidan"/>
-        </div>
-  
-        <img id="logo" src="images/nti_logo_footer.svg" alt="ntis logga"/>
-        
-        <div className="grid-center">
-            <div className="message-container-wrapper">
+            <div className="backgroundImage">
+                <img src="images/backgroundNTI.jpg" alt="bakgrunds bild på hemsidan" />
+            </div>
 
-            {(verified) ? 
-                <div id="main">
-                    <h1>Ange din status</h1>
-                    {(!people.length) ? 
-                        <h1>Laddar innehållet..</h1> 
-                        : 
-                        people 
+            <img id="logo" src="images/nti_logo_footer.svg" alt="ntis logga" />
+
+            <div className="grid-center">
+                <div className="message-container-wrapper">
+                    {(!verified) ?
+                        <>
+                            <h3>Tillträde förbjuden</h3>
+                            <p>Vänligen kolla så att din adress är korrekt</p>
+                        </>
+                        :
+                        <>
+                            <div id="main">
+                                <h1>Ange din status</h1>
+                                {(!people.length) ?
+                                    <p>Laddar innehållet..</p>
+                                    :
+                                    people
+                                }
+                            </div>
+                            <div>
+                                <Link href="/">
+                                    <a id="status-link-hover">Visa Status</a>
+                                </Link>
+                            </div>
+                        </>
                     }
                 </div>
-                : <p>Tillträde förbjuden</p>
-                }
-            
-                <div>
-                    <Link href="/">
-                        <a id="status-link-hover">Visa Status</a>
-                    </Link>
-                </div>
-            </div> 
-        </div>
-      </>     
+            </div>
+        </>
     );
-  }
-  
-  export default Input;
+}
+
+/* Before the pages loads, ask api */
+export const getServerSideProps = async (context) => {
+    if (context.query == '' || context.query === undefined) {
+        return {
+            props: {
+                verified: false,
+                users: false
+            }
+        }
+    }
+    else {
+        let res = await axios.post(`${process.env.HOST_URL}api/verifyurl`, { uri: context.query.auth })
+        return {
+            props: {
+                verified: res.data.verified,
+                users: res.data.users
+            }
+        }
+    }
+}
+export default Input;
