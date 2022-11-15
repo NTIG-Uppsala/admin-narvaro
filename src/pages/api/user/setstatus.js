8c68694f7786955bca6e8ec414fa3cec0d33d1ab
linus-jansson <contact@limpan.dev>
2022-11-15 14:10:45 +0100

    moved to seperate route

--- src/pages/api/user/setstatus.js
+++ src/pages/api/user/setstatus.js
@@ -1,41 +1,37 @@
-import Database from "../../libs/Database"
-import { isTokenValid } from "../../libs/jwt"
+import Database from "../../../libs/Database"
+import { isTokenValid } from "../../../libs/jwt"
 const database_instance = new Database();
 
 export default async function handler(req, res) {
     if (req.method !== "POST") return res.status(405).send("Method not allowed");
     let data = req.body
 
-    req.io.emit("status update", data)
+    console.log("data", data)
 
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     /* 
-        Verify request body
-        - token cant be null
-        - payload needs to have status and it has to be a boolean
+    Verify request body
+    - token cant be null
+    - payload needs to have status and it has to be a boolean
     */
     if (token === null) return res.sendStatus(401)
-    if (data.status === undefined || data.status === null) return res.sendStatus(400)
-    if (Object.hasOwn(data, 'status') && typeof data.status === "boolean") return res.sendStatus(400)
+    if (typeof data?.status !== "boolean") return res.sendStatus(400)
     try {
         let token_payload = await isTokenValid(token)
 
-        /* if token is a long lived token check in database if it still exists */
-        if (token_payload?.token_type === "long-lived") {
-            let token_database_result = await database_instance.get_token(token)
-            if (token_database_result === null) return res.sendStatus(403)
+        /* If token is role device, check if in database  */
+        let device;
+        if (token_payload.role === "device") {
+            device = await database_instance.get_device(token)
+            if (device === null) return res.sendStatus(401)
         }
 
-        let user_id = data?.id
+        let user_id = device?.user || data.id
         let new_status = data.status
 
+        req.io.emit("status update", data)
         // If token payload has a device id
-        if (token_payload.data?.device_id) {
-            // Get user associated with device from database
-            let result = await database_instance.get_device(token_payload.data.device_id)
-            user_id = result.user
-        }
         database_instance.update_user(user_id, {
             status: new_status,
             latest_change: new Date()
