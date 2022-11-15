2b6536ea42bfc0b94d340dd77465b23cea9af754
linus-jansson <contact@limpan.dev>
2022-11-15 12:56:36 +0100

    only cors for /api/get/:slug

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/:path*",
+                source: "/api/get/:slug*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
