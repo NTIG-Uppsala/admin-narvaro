e7549e39985f1e0b5191f26e0fa91993a136d006
Kasper-coder <kasper.holgersson@elev.ga.ntig.se>
2022-11-16 10:52:28 +0100

    remove db verification and stick to reading and verifying token
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>

--- src/pages/api/device/add.js
+++ src/pages/api/device/add.js
@@ -1,8 +1,10 @@
-import Database from "../../../libs/Database";
 import { isTokenValid } from "../../../libs/jwt";
 import axios from "axios";
 export default async function handler(req, res) {
+    /* If not a post request, reject the request */
     if (req.method !== "POST") return res.status(405).send("Method not allowed");
+
+    /* Check headers for authorization and verify the Bearer token */
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     try {
@@ -11,31 +13,22 @@ export default async function handler(req, res) {
         return res.sendStatus(401)
     }
 
-    const database_instance = new Database();
     const device_token_payload = {
         user_id: req.body.user,
         device_name: req.body.user,
         role: "device"
     }
 
+    /* Generate tokens */
     const device_tokens = await axios.post(`${process.env.HOST_URL}/api/auth/token`, device_token_payload, { headers: { "Authorization": `Bearer ${token}` } })
     try {
-
-        const user_id = req.body.user;
-        const device_name = req.body.user;
-
-        const device = await database_instance.add_device(device_name, user_id, device_tokens.data.refresh_token);
         let response = {
-            user: user_id,
-            device_name: device_name,
+            user: req.body.user,
+            device_name: req.body.user,
             token: device_tokens.data.refresh_token
         }
-        if (device) {
-            res.status(200).json(device);
-        }
-        else {
-            res.sendStatus(404);
-        }
+        /* Send back tokens */
+        res.status(200).json(response);
     } catch (error) {
         console.error(error);
         res.sendStatus(500)
