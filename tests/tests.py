83c787100d3f30a98fa1930010f6a2a2682a16f4
linus-jansson <contact@limpan.dev>
2022-11-16 14:58:31 +0100

    improve tests
    Co-authored-by: Kasper-coder <kasper.holgersson@elev.ga.ntig.se>

--- tests/tests.py
+++ tests/tests.py
@@ -9,11 +9,12 @@ import time
 
 class CheckWebsite(unittest.TestCase):
     website_url = "http://localhost:8000/"  # Standard URL
+    is_checked = None
     @classmethod
     def setUpClass(self):
         service = Service(executable_path=ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
         self.driver = webdriver.Chrome(service=service, options=options)
     
     @classmethod
@@ -23,20 +24,19 @@ class CheckWebsite(unittest.TestCase):
     def test_a_toggle_checkbox(self):
         self.driver.get(self.website_url + "setstatus?auth=Z2Kywv8RkQ")
         time.sleep(1)
-        checkbox = self.driver.find_element(By.CSS_SELECTOR, "#slider-635b94fc8ea8f0ec28efa1f1")
+        checkbox = self.driver.find_element(By.ID, "slider-635b94fc8ea8f0ec28efa1f1")
+        self.is_checked = not checkbox.is_selected()
         checkbox.click()
-        self.assertTrue or self.assertFalse(checkbox.is_selected())
 
     def test_b_check_status(self):
         self.driver.get(self.website_url)
         time.sleep(3)
         avalibility = self.driver.find_element(By.ID, "635b94fc8ea8f0ec28efa1f1")
 
-        if avalibility.text == "Tillgänglig":
-            self.assertTrue(True)
+        if self.is_checked:
+            self.assertEqual(avalibility.text, "Tillgänglig")
         else:
-            self.assertFalse(False)
-
+            self.assertEqual(avalibility.text, "Ej tillgänglig")
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
