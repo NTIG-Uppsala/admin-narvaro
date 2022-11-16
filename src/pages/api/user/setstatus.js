376f8b6978c453eb04d1a226fa97f31f7c9d1c1b
linus-jansson <contact@limpan.dev>
2022-11-16 11:30:43 +0100

    undefined instead of raising error

--- src/pages/api/user/setstatus.js
+++ src/pages/api/user/setstatus.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
             if (device === null) return res.sendStatus(401)
         }
 
-        let user_id = device.user || data.id
+        let user_id = device?.user || data.id
         let new_status = data.status
 
         req.io.emit("status update", data)
