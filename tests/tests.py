4e8b11d6515300e9dcbaad4ad9f8af768c4c8a1a
Carl Lagerstedt <105862361+Carl595@users.noreply.github.com>
2023-10-24 11:25:37 +0200

    Fix GitHub actions tests (#57)

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,5 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
@@ -12,28 +10,26 @@ class CheckWebsite(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        service = Service(executable_path=ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
-        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver = webdriver.Chrome(options=options)
 
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
 
     def test_a_toggle_checkbox(self):
-        self.driver.get(self.website_url + "setstatus?auth=Z2Kywv8RkQ")
+        self.driver.get(self.website_url + "setstatus?auth=kA724hAn0a")
         time.sleep(1)
-        checkbox = self.driver.find_element(By.ID, "slider-635b94fc8ea8f0ec28efa1f1")
-        self.is_checked = not checkbox.is_selected()
-        checkbox.click()
+        checkbox = self.driver.find_element(By.ID, "653284cad43c535e3026f80a")
+        self.__class__.is_checked = checkbox.is_selected()
 
     def test_b_check_status(self):
         self.driver.get(self.website_url)
         time.sleep(3)
-        avalibility = self.driver.find_element(By.ID, "635b94fc8ea8f0ec28efa1f1")
+        avalibility = self.driver.find_element(By.ID, "653284cad43c535e3026f80a")
 
-        if self.is_checked:
+        if self.__class__.is_checked:
             self.assertEqual(avalibility.text, "Tillgänglig")
         else:
             self.assertEqual(avalibility.text, "Ej tillgänglig")
