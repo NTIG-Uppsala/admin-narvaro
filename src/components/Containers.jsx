dbe19a8452617db3c1a4167f29515faca554463f
Carl595 <105862361+Carl595@users.noreply.github.com>
2023-10-25 14:23:11 +0200

    Revert "Add mb to container"
    This reverts commit 0167f731f81cd715d504f4ce277bce4c918fb327.

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -1,6 +1,6 @@
 export function Container({ children }) {
     return (
-        <div className="pt-6 mt-5 mb-5 rounded-xl bg-black/75 flex flex-col justify-center min-w-fit border-[3px] border-solid border-white">
+        <div className="pt-6 mt-5 rounded-xl bg-black/75 flex flex-col justify-center min-w-fit border-[3px] border-solid border-white">
             {children}
         </div>
     )
