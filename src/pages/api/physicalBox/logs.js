c9f00ed84b972d90c4f2824a9ab6417f18b959f6
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-11-09 10:58:45 +0100

    API endpoint for posting logs
    Co-Authored-By: Fabian-Hemstrom <142995105+Fabian-Hemstrom@users.noreply.github.com>

---
+++ src/pages/api/physicalBox/logs.js
@@ -0,0 +1,42 @@
+import { isTokenValid } from "../../../libs/jwt";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).send("Method not allowed");
+  let data = req.body;
+
+  console.log("data", data);
+
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token === null) return res.sendStatus(401);
+
+  if (typeof data?.status !== "string") return res.sendStatus(400);
+  try {
+    let token_payload = await isTokenValid(token);
+    console.log(token_payload);
+
+    let user_id = token_payload.data?.user_id || data.id;
+
+    console.log(user_id);
+
+    // let new_status = data.status
+    // req.io.emit("status update", data)
+    // // If token payload has a device id
+    // database_instance.update_user(user_id, {
+    //     status: new_status,
+    //     latest_change: new Date()
+    // })
+    //     .then((result) => {
+    //         console.log("Updated user status, result ->", result)
+    //         return res.status(200).json({})
+    //     }).catch((err) => {
+    //         console.log(err)
+    //         req.io.emit("status update", { ...data, status: !data.status, error: err })
+    //         return res.status(401).json({})
+    //     });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(403);
+  }
+}
