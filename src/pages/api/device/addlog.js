52a8e7f02ff2962d9b75ac8b7969f05e300bd85b
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-11-16 14:52:01 +0100

    Add api endpoint for recieving logs
    Co-Authored-By: Fabian-Hemstrom <142995105+Fabian-Hemstrom@users.noreply.github.com>

---
+++ src/pages/api/device/addlog.js
@@ -0,0 +1,24 @@
+import { isTokenValid } from "../../../libs/jwt";
+import axios from "axios";
+export default async function handler(req, res) {
+  /* If not a post request, reject the request */
+  if (req.method !== "POST") return res.status(405).send("Method not allowed");
+
+  /* Check headers for authorization and verify the Bearer token */
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  try {
+    let token_payload = await isTokenValid(token);
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+
+  print(req.body);
+
+  try {
+    res.status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
