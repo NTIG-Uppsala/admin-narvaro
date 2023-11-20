802de60cb3b6791973a9d384c493e0daa0791c5e
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-11-20 09:58:41 +0100

    Remove title TE4 and add 502 error response

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,110 +1,132 @@
-import axios from 'axios';
+import axios from "axios";
 import { useState, useEffect } from "react";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
-import { Container } from '../components/Containers'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Te4logo from '../components/te4logo'
+import { Container } from "../components/Containers";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Te4logo from "../components/te4logo";
 
-import Head from 'next/head'
-import moment from 'moment';
-import 'moment/locale/sv';
-moment.locale('sv')
-
+import Head from "next/head";
+import moment from "moment";
+import "moment/locale/sv";
+moment.locale("sv");
 
 const WebPage = (props) => {
-    const [people, setPeople] = useState(props.users)
-    const Person = (propscomp) => {
-        const [latest_change, setLatestChange] = useState(moment(propscomp.latest_change).fromNow());
-    
-        useEffect(() => {
-            /* Will update the "from Last Updated" text every minute */
-            let inter = setInterval(() => {
-                setLatestChange(moment(propscomp.latest_change).fromNow());
-            }, 60000);
-            return () => clearInterval(inter);
-        }, []);
-    
-        useEffect(() => {
-            setLatestChange(moment(propscomp.latest_change).fromNow());
-        }, [propscomp.latest_change]);
-    
-        const isLastChild = propscomp.isLastChild;
-    
-        return (
-            <div className={((propscomp.id % 2 === 0) ? "bg-stone-700/50" : "") + " flex flex-row pr-6 pl-6 md:gap-x-32 lg:gap-x-64 justify-between items-center py-6 px-3 md:py-3" + (isLastChild ? " rounded-b-lg" : "")}>
-                <div className="text-left mr-5">
-                    <p className="text-xl md:text-3xl break-keep">{propscomp.name}</p>
-                    <span className="text-xs md:text-sm lg:text-base">{propscomp.role}</span>
-                </div>
-                <div className="text-right">
-                    <p className={"text-xl md:text-3xl" + ((propscomp.status) ? " text-[#00ff00]" : " text-[#ff0000]")} id={propscomp._id}>
-                        {(propscomp.status) ? "Tillgänglig" : "Ej tillgänglig"}
-                    </p>
-                    <span className="text-xs md:text-sm lg:text-base">Senast Uppdaterad:<br />{latest_change}</span>
-                </div>
-            </div>
-        );
-    }
-    
-    
+  const [people, setPeople] = useState(props.users);
+  const Person = (propscomp) => {
+    const [latest_change, setLatestChange] = useState(
+      moment(propscomp.latest_change).fromNow()
+    );
 
-    const socket = io();
-
-    /* Will be runned as a client side script when the page renders */
     useEffect(() => {
-        socket.on('status update', (response) => {
-            console.log("STATUS UPDATE: \n", response)
-            axios.get('/api/get/users').then(res => {
-                console.log("data gotten from api: \n", res.data)
-                setPeople(res.data)
-            });
-        });
+      /* Will update the "from Last Updated" text every minute */
+      let inter = setInterval(() => {
+        setLatestChange(moment(propscomp.latest_change).fromNow());
+      }, 60000);
+      return () => clearInterval(inter);
     }, []);
 
+    useEffect(() => {
+      setLatestChange(moment(propscomp.latest_change).fromNow());
+    }, [propscomp.latest_change]);
+
+    const isLastChild = propscomp.isLastChild;
+
     return (
-        <>
-            <Head>
-                <title>Administrationsstatus</title>
-            </Head>
-            <Background />
-            <Logo />
+      <div
+        className={
+          (propscomp.id % 2 === 0 ? "bg-stone-700/50" : "") +
+          " flex flex-row pr-6 pl-6 md:gap-x-32 lg:gap-x-64 justify-between items-center py-6 px-3 md:py-3" +
+          (isLastChild ? " rounded-b-lg" : "")
+        }
+      >
+        <div className="text-left mr-5">
+          <p className="text-xl md:text-3xl break-keep">{propscomp.name}</p>
+          <span className="text-xs md:text-sm lg:text-base">
+            {propscomp.role}
+          </span>
+        </div>
+        <div className="text-right">
+          <p
+            className={
+              "text-xl md:text-3xl" +
+              (propscomp.status ? " text-[#00ff00]" : " text-[#ff0000]")
+            }
+            id={propscomp._id}
+          >
+            {propscomp.status ? "Tillgänglig" : "Ej tillgänglig"}
+          </p>
+          <span className="text-xs md:text-sm lg:text-base">
+            Senast Uppdaterad:
+            <br />
+            {latest_change}
+          </span>
+        </div>
+      </div>
+    );
+  };
 
-            <Head>
-                <title>TE4</title>
-            </Head>
-            <Te4logo/>
+  const socket = io();
 
-            <div className="h-screen w-full grid place-items-center">
-                <Container>
-                    <h1 className="font-lg text-5xl font-bold text-center">Status</h1>
-                    <noscript>
-                        <p className="text-center">Du har inte javascript aktiverat. Detta kan orsaka att webplatsen inte fungerar som den ska</p>
-                    </noscript>
-                    <div className="flex flex-col md:pt-12">
-                        {people && people.map((item, index) => {
-                        const isLastChild = index === people.length - 1;
-                        return <Person key={index} id={index + 1} isLastChild={isLastChild} {...item} />;
-                    })}
-</div>
+  /* Will be runned as a client side script when the page renders */
+  useEffect(() => {
+    socket.on("status update", (response) => {
+      console.log("STATUS UPDATE: \n", response);
+      axios.get("/api/get/users").then((res) => {
+        console.log("data gotten from api: \n", res.data);
+        setPeople(res.data);
+      });
+    });
+  }, []);
 
-                </Container>
+  return (
+    <>
+      <Head>
+        <title>Administrationsstatus</title>
+      </Head>
+      <Background />
+      <Logo />
 
-            </div>
+      <Te4logo />
 
-        </>
-    )
-}
+      <div className="h-screen w-full grid place-items-center">
+        <Container>
+          <h1 className="font-lg text-5xl font-bold text-center">Status</h1>
+          <noscript>
+            <p className="text-center">
+              Du har inte javascript aktiverat. Detta kan orsaka att webplatsen
+              inte fungerar som den ska
+            </p>
+          </noscript>
+          <div className="flex flex-col md:pt-12">
+            {people &&
+              people.map((item, index) => {
+                const isLastChild = index === people.length - 1;
+                return (
+                  <Person
+                    key={index}
+                    id={index + 1}
+                    isLastChild={isLastChild}
+                    {...item}
+                  />
+                );
+              })}
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
 
 export async function getServerSideProps(context) {
-    let response = await axios.get(`${process.env.HOST_URL}api/get/users`)
+  let response = await axios.get(`${process.env.HOST_URL}api/get/users`);
 
-    return {
-        props: {
-            users: response.data
-        }
-    }
+  return {
+    props: {
+      users: response.data,
+    },
+  };
 }
 
 export default WebPage;
--- src/server.js
+++ src/server.js
@@ -1,43 +1,48 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 if (process.env.HOST_URL == undefined) {
-    throw "HOST_URL is required as a variable in .env (eg. HOST_URL=http://localhost:3000/)";
+  throw "HOST_URL is required as a variable in .env (eg. HOST_URL=http://localhost:3000/)";
 }
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
 
-import express from 'express'
-import bodyParser from 'body-parser';
-import next from 'next';
-import http from 'http';
-import { Server as SocketServer } from 'socket.io'
+import express from "express";
+import bodyParser from "body-parser";
+import next from "next";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
 
 const server = express();
 const http_server = http.createServer(server);
 const io = new SocketServer(http_server);
 
-const nextApp = next({ dev })
+const nextApp = next({ dev });
 
 nextApp.prepare().then(async () => {
+  /* Set up body-parser */
+  server.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
 
-    /* Set up body-parser */
-    server.use(bodyParser.urlencoded({
-        extended: true
-    }));
+  server.use(express.json());
 
-    server.use(express.json());
+  /* Required for socker server to work with API routes */
+  server.use((req, res, _next) => {
+    req.io = io;
+    _next();
+  });
 
-    /* Required for socker server to work with API routes */
-    server.use((req, res, _next) => {
-        req.io = io;
-        _next();
-    });
+  app.use((req, res, next) => {
+    res.status(502).sendFile(__dirname + "/public/error.html");
+  });
 
-    /* Handle all requests through next */
-    server.use((req, res) => nextApp.getRequestHandler()(req, res))
+  /* Handle all requests through next */
+  server.use((req, res) => nextApp.getRequestHandler()(req, res));
 
-    /* Listen on port 8000 */
-    http_server.listen(8000, (err) => {
-        if (err) throw err
-        console.log("Server is running on port 8000")
-    });
+  /* Listen on port 8000 */
+  http_server.listen(8000, (err) => {
+    if (err) throw err;
+    console.log("Server is running on port 8000");
+  });
 });
